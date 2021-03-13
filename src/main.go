36c2bc47df0b4d3b925f03d2ede964814f9c8597
atyu1 <attila.sovak@mail.com>
2021-03-13 23:42:48 +0100

    Add template handler

--- src/main.go
+++ src/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Just intro data")
+type Page struct {
+	Title string
+	Text  string
+}
+
+func homeViewHandler(w http.ResponseWriter, r *http.Request) {
+	text := r.URL.Path[len("/"):]
+	p, err := loadPage(text)
+	if err != nil {
+		p = &Page{Title: "Home", Text: text}
+	}
+}
+
+var templates = template.Must(template.ParseFiles("home.htmltmpl"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".htmltmpl", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^/(home)/([a-zA-Z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/home", makeHandler((homeViewHandler))
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
